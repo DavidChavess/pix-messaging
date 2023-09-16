4c8e42009166e5ad8dbff975329a0ec86bb43599
DavidChavess <davi.ch.fe@gmail.com>
2023-09-16 11:22:22 -0300

    first commit

---
+++ pix-producer-api/src/main/kotlin/com/davidchaves/pixproducerapi/PixProducerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.davidchaves.pixproducerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PixProducerApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PixProducerApiApplication>(*args)
+}
---
+++ pix-producer-api/src/test/kotlin/com/davidchaves/pixproducerapi/PixProducerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.davidchaves.pixproducerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PixProducerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
