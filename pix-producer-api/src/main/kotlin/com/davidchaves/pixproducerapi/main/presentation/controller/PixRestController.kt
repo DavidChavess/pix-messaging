c9e0346cbc7f58fffa58dd71a0b349258d58609d
DavidChavess <davi.ch.fe@gmail.com>
2023-09-23 20:06:33 -0300

    feat: add status created when create a pix

--- pix-producer-api/src/main/kotlin/com/davidchaves/pixproducerapi/main/presentation/controller/PixRestController.kt
+++ pix-producer-api/src/main/kotlin/com/davidchaves/pixproducerapi/main/presentation/controller/PixRestController.kt
@@ -3,6 +3,7 @@ package com.davidchaves.pixproducerapi.main.presentation.controller
 import com.davidchaves.pixproducerapi.domain.usecase.CreatePixUseCase
 import com.davidchaves.pixproducerapi.domain.usecase.FindPixByIdUseCase
 import com.davidchaves.pixproducerapi.domain.usecase.PixDto
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,6 +13,7 @@ class PixRestController(
         private val findPixByIdUseCase: FindPixByIdUseCase,
 ) {
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody pixDto: PixDto) = createPixUseCase.create(pixDto)
 
     @GetMapping("/{id}")
