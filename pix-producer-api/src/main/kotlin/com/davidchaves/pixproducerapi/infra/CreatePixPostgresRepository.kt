8543b7d614d7625af31d3c88786f6f487d0e829c
DavidChavess <davi.ch.fe@gmail.com>
2023-10-01 10:36:55 -0300

    feat: add update function in method add of CreatePixPostgresRepository

--- pix-producer-api/src/main/kotlin/com/davidchaves/pixproducerapi/infra/CreatePixPostgresRepository.kt
+++ pix-producer-api/src/main/kotlin/com/davidchaves/pixproducerapi/infra/CreatePixPostgresRepository.kt
@@ -8,19 +8,20 @@ import com.davidchaves.pixproducerapi.infra.postgres.PixRepository
 
 class CreatePixPostgresRepository(private val pixRepository: PixRepository) : CreatePixRepository {
 
-    override fun add(pix: CreatePix): CreatePix {
-        return pix
-            .run {
-                PixEntity(
-                    identifier = id,
-                    targetKey = targetKey,
-                    sourceKey = sourceKey,
-                    value = value,
-                    transferDate = transferDate,
-                    status = status.name
-                )
-            }
-            .run(pixRepository::save)
-            .run { CreatePix(identifier, targetKey, sourceKey, value, transferDate, PixStatus.valueOf(status)) }
-    }
+    override fun add(pix: CreatePix): CreatePix = pixRepository.findByIdentifier(pix.id)
+        .map(PixEntity::id).orElse(null)
+        .let { toPixEntity(it, pix) }
+        .run(pixRepository::save)
+        .run { CreatePix(identifier, targetKey, sourceKey, value, transferDate, PixStatus.valueOf(status)) }
+
+
+    private fun toPixEntity(id: Long?, pix: CreatePix) = PixEntity(
+        id,
+        identifier = pix.id,
+        targetKey = pix.targetKey,
+        sourceKey = pix.sourceKey,
+        value = pix.value,
+        transferDate = pix.transferDate,
+        status = pix.status.name
+    )
 }
